38517f5cb49cfc5bc7ffe3f2c03144f24b058585
zzzherick <herick9898@hotmail.com>
2018-12-28 13:22:59 -0200

    Adicionar arquivos de projeto.

---
+++ Turing/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Turing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxDiv = new System.Windows.Forms.CheckBox();
+            this.boxIgualdade = new System.Windows.Forms.CheckBox();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.textNum1 = new System.Windows.Forms.TextBox();
+            this.textNum2 = new System.Windows.Forms.TextBox();
+            this.textFita = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lblOperador = new System.Windows.Forms.Label();
+            this.lblSeparator = new System.Windows.Forms.Label();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // boxDiv
+            // 
+            this.boxDiv.AutoSize = true;
+            this.boxDiv.Location = new System.Drawing.Point(12, 12);
+            this.boxDiv.Name = "boxDiv";
+            this.boxDiv.Size = new System.Drawing.Size(61, 17);
+            this.boxDiv.TabIndex = 0;
+            this.boxDiv.Text = "Divisão";
+            this.boxDiv.UseVisualStyleBackColor = true;
+            this.boxDiv.CheckedChanged += new System.EventHandler(this.boxDiv_CheckedChanged);
+            // 
+            // boxIgualdade
+            // 
+            this.boxIgualdade.AutoSize = true;
+            this.boxIgualdade.Location = new System.Drawing.Point(98, 12);
+            this.boxIgualdade.Name = "boxIgualdade";
+            this.boxIgualdade.Size = new System.Drawing.Size(73, 17);
+            this.boxIgualdade.TabIndex = 1;
+            this.boxIgualdade.Text = "Igualdade";
+            this.boxIgualdade.UseVisualStyleBackColor = true;
+            this.boxIgualdade.CheckedChanged += new System.EventHandler(this.boxIgualdade_CheckedChanged);
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(177, 33);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnAplicar.TabIndex = 2;
+            this.btnAplicar.Text = "Aplicar";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // textNum1
+            // 
+            this.textNum1.Location = new System.Drawing.Point(12, 35);
+            this.textNum1.Name = "textNum1";
+            this.textNum1.Size = new System.Drawing.Size(61, 20);
+            this.textNum1.TabIndex = 3;
+            this.textNum1.Text = "nº 1";
+            // 
+            // textNum2
+            // 
+            this.textNum2.BackColor = System.Drawing.Color.White;
+            this.textNum2.Location = new System.Drawing.Point(98, 35);
+            this.textNum2.Name = "textNum2";
+            this.textNum2.Size = new System.Drawing.Size(61, 20);
+            this.textNum2.TabIndex = 4;
+            this.textNum2.Text = "nº 2";
+            this.textNum2.TextChanged += new System.EventHandler(this.textNum2_TextChanged);
+            // 
+            // textFita
+            // 
+            this.textFita.Location = new System.Drawing.Point(12, 87);
+            this.textFita.Multiline = true;
+            this.textFita.Name = "textFita";
+            this.textFita.Size = new System.Drawing.Size(557, 26);
+            this.textFita.TabIndex = 5;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(12, 119);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 6;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lblOperador
+            // 
+            this.lblOperador.AutoSize = true;
+            this.lblOperador.Location = new System.Drawing.Point(79, 38);
+            this.lblOperador.Name = "lblOperador";
+            this.lblOperador.Size = new System.Drawing.Size(13, 13);
+            this.lblOperador.TabIndex = 7;
+            this.lblOperador.Text = "?";
+            // 
+            // lblSeparator
+            // 
+            this.lblSeparator.AutoSize = true;
+            this.lblSeparator.Location = new System.Drawing.Point(12, 59);
+            this.lblSeparator.Name = "lblSeparator";
+            this.lblSeparator.Size = new System.Drawing.Size(247, 13);
+            this.lblSeparator.TabIndex = 8;
+            this.lblSeparator.Text = "________________________________________";
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(12, 148);
+            this.textArea.Name = "textArea";
+            this.textArea.Size = new System.Drawing.Size(557, 226);
+            this.textArea.TabIndex = 9;
+            this.textArea.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 386);
+            this.Controls.Add(this.textArea);
+            this.Controls.Add(this.lblSeparator);
+            this.Controls.Add(this.lblOperador);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.textFita);
+            this.Controls.Add(this.textNum2);
+            this.Controls.Add(this.textNum1);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.boxIgualdade);
+            this.Controls.Add(this.boxDiv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox boxDiv;
+        private System.Windows.Forms.CheckBox boxIgualdade;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.TextBox textNum1;
+        private System.Windows.Forms.TextBox textNum2;
+        private System.Windows.Forms.TextBox textFita;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label lblOperador;
+        private System.Windows.Forms.Label lblSeparator;
+        private System.Windows.Forms.RichTextBox textArea;
+    }
+}
+
---
+++ Turing/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Turing
+{
+
+    public partial class Form1 : Form
+    {
+        private Boolean div = false;
+        private Boolean igual = false;
+        private List<string> lista = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boxDiv_CheckedChanged(object sender, EventArgs e)
+        {
+            boxIgualdade.Checked = false;
+            lblOperador.Text = "?";
+            textNum1.Text = "nº 1";
+            textNum2.Text = "nº 2";
+            if (boxDiv.Checked == true)
+            {
+                textNum1.Text = "";
+                textNum2.Text = "";
+                this.igual = false;
+                this.div = true;
+                lblOperador.Text = "/";
+            }
+        }
+
+        private void boxIgualdade_CheckedChanged(object sender, EventArgs e)
+        {
+            boxDiv.Checked = false;
+            lblOperador.Text = "?";
+            textNum1.Text = "nº 1";
+            textNum2.Text = "nº 2";
+            if (boxIgualdade.Checked == true)
+            {
+                textNum1.Text = "";
+                textNum2.Text = "";
+                this.div = false;
+                this.igual = true;
+                lblOperador.Text = "=";
+            }
+        }
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            if (div == false && igual == false)
+            {
+                MessageBox.Show("Selecione uma das caixas", "Máquina de Turing",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+            if(textNum1.TextLength == 0 || textNum2.TextLength == 0)
+            {
+                MessageBox.Show("Digite algum número!", "Máquina de Turing",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            int num1 = Convert.ToInt32(textNum1.Text);
+            int num2 = Convert.ToInt32(textNum2.Text);
+
+            if ((num2 > num1) && div == true)
+            {
+                MessageBox.Show("O segundo número deve ser menor ou igual ao primeiro", "Máquina de Turing",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                return;
+            }
+            textFita.Text = "";
+            textArea.Text = "";
+            lista = new List<string>();
+
+            preparaFita(num1, num2);
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            if (div == false && igual == false)
+            {
+                MessageBox.Show("Selecione uma das caixas", "Máquina de Turing",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                if (div)
+                {
+                    Div();
+                }
+                else if (igual)
+                {
+                    Igual();
+                }
+            }
+        }
+
+        public void Div()
+        {
+            textFita.Text = "";
+
+            int estado = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                string item = lista[i];
+
+                switch (estado)
+                {
+                    case 0:
+                        if (item.Equals(">")) { estado = 1; }
+                        break;
+
+                    case 1:
+                        if (item.Equals("*")) { estado = 1; }
+                        if (item.Equals("_")) { estado = 2; }
+                        break;
+
+                    case 2:
+                        if (item.Equals("*"))
+                        {
+                            estado = 3;
+                            lista[i] = "y";
+                            manipularArea();
+                            i = i - 2;
+                        }
+                        if (item.Equals("_")) { estado = 5; lista.Add("_"); manipularArea(); }
+                        if (item.Equals("y")) { estado = 2; }
+                        break;
+
+                    case 3:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { i = i - 2; }
+                        if (item.Equals("y")) { i = i - 2; }
+                        if (item.Equals("z")) { estado = 4; }
+                        if (item.Equals(">")) { estado = 4; }
+                        break;
+
+                    case 4:
+                        if (item.Equals("*"))
+                        {
+                            estado = 1;
+                            lista[i] = "y";
+                            manipularArea();
+                        }
+                        if (item.Equals("_")) { estado = 9; i = i - 2; }
+                        if (item.Equals("y")) { estado = 4; }
+                        break;
+
+                    case 5:
+                        if (item.Equals("*")) { estado = 5; }
+                        if (item.Equals("_"))
+                        {
+                            estado = 6;
+                            lista[i] = "*";
+                            manipularArea();
+                            i = i - 2;
+                        }
+                        break;
+
+                    case 6:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { estado = 7; i = i - 2; }
+                        break;
+
+                    case 7:
+                        if (item.Equals("_")) { estado = 8; i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); i = i - 2; }
+                        break;
+
+                    case 8:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "z"; estado = 1; manipularArea(); }
+                        break;
+
+                    case 9:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; estado = 10; manipularArea(); }
+                        if (item.Equals("z"))
+                        {
+                            estado = 17;
+                            lista[i] = "*";
+                            manipularArea();
+                            i = i - 2;
+                        }
+                        break;
+
+                    case 10:
+                        if (item.Equals("_")) { estado = 11; }
+                        break;
+
+                    case 11:
+                        if (item.Equals("_")) { estado = 12; }
+                        if (item.Equals("y")) { lista[i] = "*"; lista.Add("_"); manipularArea(); }
+                        break;
+
+                    case 12:
+                        if (item.Equals("_")) { estado = 13; /*lista.Add("_"); */}
+                        break;
+
+                    case 13:
+                        if (item.Equals("_")) { lista[i] = "*"; estado = 14; manipularArea(); i = i - 2; }
+                        break;
+
+                    case 14:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { estado = 15; i = i - 2; }
+                        break;
+
+                    case 15:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { estado = 16; i = i - 2; }
+                        break;
+
+                    case 16:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { estado = 9; i = i - 2; }
+                        break;
+
+                    case 17:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); i = i - 2; }
+                        if (item.Equals("z")) { lista[i] = "*"; manipularArea(); i = i - 2; }
+                        if (item.Equals(">")) { estado = 18; /*i = lista.Count;*/ }
+                        break;
+
+                    case 18:
+                        if (item.Equals("_")) { estado = 19; }
+                        break;
+
+                    case 19:
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); }
+                        if (item.Equals("_")) { i = lista.Count; }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            manipularFita();
+        }
+
+        public void Igual()
+        {
+            textFita.Text = "";
+
+            int estado = -1;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                string item = lista[i];
+
+                switch (estado)
+                {
+                    case -1:
+                        if (item.Equals(">")) { estado = 0; }
+                        break;
+                    case 0:
+                        if (item.Equals("*")) { lista[i] = "y"; manipularArea(); estado = 1; }
+                        if (item.Equals("_")) { estado = 5; }
+                        break;
+                    case 1:
+                        if (item.Equals("_")) { estado = 2; }
+                        break;
+                    case 2:
+                        if (item.Equals("_")) { estado = 3; i = i - 2; }
+                        if (item.Equals("y")) { estado = 3; i = i - 2; }
+                        break;
+                    case 3:
+                        if (item.Equals("*")) { lista[i] = "y"; manipularArea(); estado = 4; i = i - 2; }
+                        if (item.Equals("_")) { estado = 10; lista.Add("_"); }
+                        break;
+                    case 4:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { i = i - 2; }
+                        if (item.Equals("y")) { estado = 0; }
+                        break;
+                    case 5:
+                        if (item.Equals("*")) { estado = 6; }
+                        if (item.Equals("_")) { estado = 7; i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); }
+                        break;
+                    case 6:
+                        if (item.Equals("_")) { estado = 8; i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); }
+                        break;
+                    case 7:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); i = i - 2; }
+                        if (item.Equals(">")) { /* FIM */  i = lista.Count; }
+                        break;
+                    case 8:
+                        if (item.Equals("*")) { lista[i] = "_"; manipularArea(); estado = 9; i = i - 2; }
+                        break;
+                    case 9:
+                        if (item.Equals("*")) { i = i - 2; }
+                        if (item.Equals("_")) { i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); i = i - 2; }
+                        if (item.Equals(">")) { estado = 0; }
+                        break;
+                    case 10:
+                        if (item.Equals("_")) { lista[i] = "*"; manipularArea(); estado = 9; i = i - 2; }
+                        if (item.Equals("y")) { lista[i] = "*"; manipularArea(); estado = 10; }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            manipularFita();
+        }
+
+        public void manipularFita()
+        {
+            textFita.Text = "";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                string item = lista[i];
+                if (item.Equals("y") || item.Equals("z"))
+                {
+                    item = "*";
+                }
+                textFita.Text += item;
+            }
+        }
+
+        public void manipularArea()
+        {
+            string item = "";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                item += lista[i];
+            }
+            textArea.Text += item;
+            textArea.Text += "\n";
+        }
+
+        public List<string> preparaFita(int num1, int num2)
+        {
+            textFita.Font = new Font("Arial", 14);
+            textArea.Font = new Font("Arial", 14);
+            lista.Add(">");
+
+            for (int i = 0; i < num1; i++)
+            {
+                lista.Add("*");
+            }
+            lista.Add("_");
+            for (int i = 0; i < num2; i++)
+            {
+                lista.Add("*");
+            }
+            lista.Add("_");
+            foreach (var item in lista)
+            {
+                textFita.Text += item.ToString();
+            }
+            return lista;
+        }
+
+        private void textNum2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
